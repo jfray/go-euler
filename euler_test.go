48ac070e1678dd75224cf56715569985fce8d6d5
Josh Fraser <josh@threefoldphotos.com>
2013-06-29 16:59:08 -0700

    refactored to have more traditional camelcased functions. getting testing up to date.

--- euler_test.go
+++ euler_test.go
@@ -17,3 +17,31 @@ func TestProjectOne(t *testing.T) {
 		t.Errorf("got %d expected %d", got, expected)
 	}
 }
+
+func BenchmarkProjectTwo(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		ProjectTwo(4000000)
+	}
+}
+
+func TestProjectTwo(t *testing.T) {
+	expected := 4613732
+	got := ProjectTwo(4000000)
+	if expected != got {
+		t.Errorf("got %d expected %d", got, expected)
+	}
+}
+
+func BenchmarkProjectThree(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		ProjectThree(6857)
+	}
+}
+
+func TestProjectThree(t *testing.T) {
+	expected := 6857
+	got := ProjectThree(600851475143)
+	if expected != got {
+		t.Errorf("got %d expected %d", got, expected)
+	}
+}
