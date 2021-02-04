fb0cc8d9bc46f7cb636ebb8699489c57da994e83
Joshua Fraser <joshua.fraser@gmail.com>
2021-02-04 15:04:04 -0800

    Update project_six.go
    tyop

--- project_six.go
+++ project_six.go
@@ -5,10 +5,10 @@ import "fmt"
 func ProjectSix(maximum int) int {
 	/*
 		The sum of the squares of the first ten natural numbers is,
-		12 + 22 + ... + 102 = 385
+		1^2 + 2^2 + ... + 10^2 = 385
 		The square of the sum of the first ten natural numbers is,
 		(1 + 2 + ... + 10)^2 = 55^2 = 3025
-		Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025  385 = 2640.
+		Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 - 385 = 2640.
 		Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
 	*/
 	mysum, square := 1, 1
